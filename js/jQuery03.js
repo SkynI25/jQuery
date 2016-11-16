7387215a431d0dc90986080ba35ec69ec1be41f5
박기태 <rlxo1651@daum.net>
2016-11-16 20:38:02 +0900

    jQuery 3번째 시간. 셀렉터 & 메소드 학습함

---
+++ js/jQuery03.js
@@ -0,0 +1,5 @@
+
+    function myFunc() {
+        alert($("select > option:selected").val()); // selected 지금 현재 선택된 것을 표시함, 사용자가 선택한것을 알수 있게 한다, 사용자가 선택하거나 입력하는 것은 val을 이용한다
+
+    }
---
+++ js/jQuery04.js
@@ -0,0 +1,5 @@
+
+    function myFunc() {
+        //alert($("[type=checkbox]:checked + span").text());
+        $("[type=checkbox]:checked").parent().remove();
+    }
